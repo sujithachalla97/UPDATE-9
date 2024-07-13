1085b6758f68125237fdfdc809f692089c3b1300
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2024-07-13 22:55:52 +0530

    Create execom.js

---
+++ execom.js
@@ -0,0 +1,46 @@
+/*document.getElementById('menu-icon').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
+});*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+            const menu = document.getElementById('menu-icon');
+            const cls = document.getElementsByClassName('navbar');
+
+            menu.onclick = function() {
+                for (let i = 0; i < cls.length; i++) {
+                    cls[i].style.display = cls[i].style.display === 'block' ? 'none' : 'block';
+                }
+               
+            };
+        });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+        duration: 1200, // Animation duration
+        easing: 'ease-in-out', // Easing function
+        once: true, // Whether animation should happen only once - while scrolling down
+        mirror: false // Whether elements should animate out while scrolling past them
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    const revealObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    reveals.forEach(reveal => {
+        revealObserver.observe(reveal);
+    });
+});
+
